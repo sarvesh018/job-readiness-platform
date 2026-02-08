75b01ed93b4f5ace7beee9f362dae9458230f580
Sarvesh <sarveshc111@gmail.com>
2026-02-08 22:32:23 +0530

    Initial backend setup

---
+++ backend/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.json({status: "UP"});
+});
+
+module.exports = app;
