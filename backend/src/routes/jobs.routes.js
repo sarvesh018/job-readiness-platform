292cdbcba5d61bc3c1df62fe169e2dff2b6a78a8
Sarvesh <sarveshc111@gmail.com>
2026-02-09 23:31:14 +0530

    sample route added

--- backend/src/routes/jobs.routes.js
+++ backend/src/routes/jobs.routes.js
@@ -4,5 +4,6 @@ const jobsController = require("../controllers/jobs.controller");
 
 router.post("/search", jobsController.searchJobs);
 router.post("/compare", jobsController.compareJD);
+router.post("/compareResume", jobsController.compareJD);
 
 module.exports = router;
