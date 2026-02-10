fa519619bd892aacba16e31fc8f1be90af88dae0
Sarvesh <sarveshc111@gmail.com>
2026-02-10 23:22:09 +0530

    remove compareResume service

--- backend/src/routes/jobs.routes.js
+++ backend/src/routes/jobs.routes.js
@@ -4,6 +4,5 @@ const jobsController = require("../controllers/jobs.controller");
 
 router.post("/search", jobsController.searchJobs);
 router.post("/compare", jobsController.compareJD);
-router.post("/compareResume", jobsController.compareJD);
 
 module.exports = router;
